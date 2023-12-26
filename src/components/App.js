e57d2cbbb06709b0fc6db65c6735073950730a77
YashSharma412 <sharmayashsatiish@gmail.com>
2023-12-26 20:42:12 +0530

    fixed UI with CSS

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 import UserList from "./UserList";
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-        <h1>Under Construction</h1>
-        <UserList />
+      {/* Do not remove the main div */}
+      <UserList />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,53 +1,58 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const UserList = ()=>{
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+  function getUsers() {
+    axios
+      .get("https://reqres.in/api/users")
+      .then((response) => {
+        setUsers(response.data.data);
+      })
+      .catch((err) => console.log(err));
+  }
 
-    const [users, setUsers] = useState([]);
-    // useEffect(()=>{
-    //     console.log(users)
-    // }, [users])
-    function getUsers(){
-        axios.get("https://reqres.in/api/users")
-        .then(response => {setUsers(response.data.data)})
-        .catch(err=>console.log(err))
-    }
-    
-    return(
-        <div>
-            <div>
-                <span>Blue Whales</span>
-                <button className="btn" onClick={getUsers}>Get User List</button>
-            </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th>Avatar</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        users.length > 0 ? (
-                            users.map((user, index)=>(
-                                <tr key={user.id}>
-                                    <td>{user.first_name}</td>
-                                    <td>{user.last_name}</td>
-                                    <td>{user.email}</td>
-                                    <td><img src={user.avatar} alt={user.first_name+ " " + user.last_name} /></td>
-                                </tr>
-                            ))
-                        ):( <tr>
-                                <td>No data found to display.</td>
-                            </tr>
-                            )
-                    }
-                </tbody>
-            </table>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="disp_flex_between">
+        <h2>Blue Whales</h2>
+        <button className="btn" onClick={getUsers}>
+          Get User List
+        </button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.length > 0 ? (
+            users.map((user, index) => (
+              <tr key={user.id}>
+                <td>{user.first_name}</td>
+                <td>{user.last_name}</td>
+                <td>{user.email}</td>
+                <td>
+                  <img
+                    src={user.avatar}
+                    alt={user.first_name + " " + user.last_name}
+                  />
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td className="err_msg" colSpan={4}>No data found to display.</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default UserList
+export default UserList;
